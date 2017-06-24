23251c1b5b8260d37ab33c7ce66cf5663fdac3a7
abiduzz420 <abiduzair420@gmail.com>
2017-06-24 15:11:22 +0530

    camera starts on launch

--- app/src/main/java/com/example/abiduzair/demohack/MainActivity.java
+++ app/src/main/java/com/example/abiduzair/demohack/MainActivity.java
@@ -6,33 +6,37 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+//import android.view.View;
+//import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+//    Button button;
     ImageView imgView;
     static final int CAM_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.button);
+//        button = (Button) findViewById(R.id.button);
         imgView = (ImageView) findViewById(R.id.image_view);
-        button.setOnClickListener(new View.OnClickListener(){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File file = getFile();
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+        startActivityForResult(cameraIntent,CAM_REQUEST);
+//        button.setOnClickListener(new View.OnClickListener(){
 
-            @Override
-            public void onClick(View v) {
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                File file = getFile();
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-                startActivityForResult(cameraIntent,CAM_REQUEST);
-            }
-        });
+//            @Override
+//            public void onClick(View v) {
+//                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                File file = getFile();
+//                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+//                startActivityForResult(cameraIntent,CAM_REQUEST);
+//            }
+//        });
     }
     @SuppressWarnings("all")
     private File getFile(){
@@ -47,6 +51,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         String path = "sdcard/camera_app/cam_image.jpg";
         imgView.setImageDrawable(Drawable.createFromPath(path));
-
     }
 }
